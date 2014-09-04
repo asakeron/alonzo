90404a0e634f529fc3c0f9242f1192f39dfd39e5
André Barnabá <asakeron@gmail.com>
2014-09-04 20:06:11 -0300

    Fixed warnings in tests.

--- tests/Main.hs
+++ tests/Main.hs
@@ -5,12 +5,20 @@ import Test.Framework.Providers.QuickCheck2
 import Test.Framework.Providers.HUnit
 import Test.HUnit
 
+main :: IO ()
 main = defaultMain [
          testGroup "Silly tests" [
-            testCase     "sum"           (2 + 2 @?= 4)
+            testCase     "sum"           case_sum
          ,  testProperty "commutativity" prop_commutativity
          ]
   ]
+case_sum :: Assertion
+case_sum = a + a @?= b
+           where
+               a :: Int
+               a = 2
+               b :: Int
+               b = 4
 
 prop_commutativity     :: Int -> Int -> Bool
 prop_commutativity x y =  x + y == y + x
