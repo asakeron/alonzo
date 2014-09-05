5b42a680ce78396bf824aa5ebf33e5e40f6244d1
André Barnabá <asakeron@gmail.com>
2014-09-05 20:35:35 -0300

    Simple REPL.

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
-main :: IO()
-main = putStrLn "oi"
+import qualified Data.Text.IO       as TextIO
+import qualified Alonzo.Parser      as Parser
+import qualified Alonzo.Interpreter as Interpreter
+import qualified Data.Map           as Map
+
+main :: IO ()
+main = do
+  expression <- TextIO.getLine
+  _          <- putStrLn $ case Parser.parse expression of
+                             Left err         -> err
+                             Right parsed     -> case Interpreter.eval (Right parsed) Map.empty of
+                                                   Right evaluated -> show evaluated
+                                                   Left  err       -> err
+  main
