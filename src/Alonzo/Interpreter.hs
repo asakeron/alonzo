691da5d3abef905c1ef08a2018e4c381e62e1bb6
asakeron <asakeron@gmail.com>
2014-09-06 22:50:30 -0300

    eval no longer takes an environment as argument.

--- src/Alonzo/Interpreter.hs
+++ src/Alonzo/Interpreter.hs
@@ -1,15 +1,19 @@
-module Alonzo.Interpreter where
+module Alonzo.Interpreter (
+  eval) where
 
 import qualified Alonzo.Syntax as Syntax
 import qualified Data.Map      as Map
 
-eval :: Syntax.Expression -> Map.Map String Syntax.Expression -> Either String Syntax.Expression
-eval a@(Syntax.Abstraction _ _) _   = return a
-eval n@(Syntax.Name identifier) env = return $ Map.findWithDefault n identifier env
-eval (Syntax.Application f arg) env = do
-                                        abstraction <- eval f env
-                                        case abstraction of
-                                          (Syntax.Abstraction name body) -> eval body $ Map.insert name arg env
-                                          _                              -> Left $ "Error: trying to apply " ++ show f ++
-                                                                                   ", which is not an abstraction, to argument "
-                                                                                   ++ show arg ++ "."
+eval :: Syntax.Expression -> Either String Syntax.Expression
+eval e = evalWithEnv e Map.empty
+
+evalWithEnv :: Syntax.Expression -> Map.Map String Syntax.Expression -> Either String Syntax.Expression
+evalWithEnv a@(Syntax.Abstraction _ _) _   = return a
+evalWithEnv n@(Syntax.Name identifier) env = return $ Map.findWithDefault n identifier env
+evalWithEnv (Syntax.Application f arg) env = do
+  abstraction <- evalWithEnv f env
+  case abstraction of
+   (Syntax.Abstraction name body) -> evalWithEnv body $ Map.insert name arg env
+   _                              -> Left $ "Error: trying to apply " ++ show f ++
+                                     ", which is not an abstraction, to argument "
+                                     ++ show arg ++ "."
