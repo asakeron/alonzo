9b2b3d39a6863a7134578720a508ca43d116ce71
asakeron <asakeron@gmail.com>
2014-09-06 22:52:56 -0300

    fixed Main to match API changes.

--- src/Main.hs
+++ src/Main.hs
@@ -3,14 +3,13 @@ module Main where
 import qualified Data.Text.IO       as TextIO
 import qualified Alonzo.Parser      as Parser
 import qualified Alonzo.Interpreter as Interpreter
-import qualified Data.Map           as Map
 
 main :: IO ()
 main = do
   expression <- TextIO.getLine
   _          <- putStrLn $ case Parser.parse expression of
                              Left err         -> err
-                             Right parsed     -> case Interpreter.eval (Right parsed) Map.empty of
+                             Right parsed     -> case Interpreter.eval parsed of
                                                    Right evaluated -> show evaluated
                                                    Left  err       -> err
   main
