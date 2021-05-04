20115090ca3e32cda7c538886a6d54faa9764645
André <abs@emancipe.se>
2021-05-04 03:34:45 -0300

    test eval and read

--- test/Main.hs
+++ test/Main.hs
@@ -1,10 +1,22 @@
 module Main where
 
-import Prelude (IO)
-
-import Control.Applicative
+import Prelude hiding (read)
+import Test.Hspec
 
 import Lambda
 
 main :: IO ()
-main = pure ()
+main = hspec $ do
+  let x  = Symbol 'x'
+      id = Abstraction x x
+      y = Evaluation id (Symbol 'y')
+  describe "read" $ do
+    it "parses a Lambda term" $ do
+      read "x"         `shouldBe` x
+      read "\\x.x"     `shouldBe` id
+      read "(\\x.x y)" `shouldBe` y
+  describe "eval" $ do
+    it "normalizes a lambda term" $ do
+      eval [] x  `shouldBe` Symbol 'x'
+      eval [] id `shouldBe` Abstraction x x
+      eval [] y  `shouldBe` Symbol 'y'
