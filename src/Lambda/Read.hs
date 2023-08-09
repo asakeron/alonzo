509a16926505b084fa25e854c2bd70a72a085323
André <abs@emancipe.se>
2023-08-08 21:09:31 -0300

    replace `parsec` with `megaparsec`

--- src/Lambda/Read.hs
+++ src/Lambda/Read.hs
@@ -1,28 +1,28 @@
 module Lambda.Read where
 
-import Text.Parsec
-
+import Text.Megaparsec
+import Text.Megaparsec.Char
 import Lambda.AST
---
+
 read :: String -> Expression
 read ts = case parse parse_ "" ts of
             Left _  -> Symbol '\0'
             Right e -> e
 
-parse_ :: Parsec String () Expression
+parse_ :: Parser Expression
 parse_ = parseSymbol_ <|> parseAbstraction_ <|> parseEvaluation_
 
-parseSymbol_ :: Parsec String () Expression
-parseSymbol_ = Symbol <$> letter
+parseSymbol_ :: Parser Expression
+parseSymbol_ = Symbol <$> letterChar
 
-parseAbstraction_ :: Parsec String () Expression
+parseAbstraction_ :: Parser Expression
 parseAbstraction_ = do
   _ <- char '\\'
   symbol <- parseSymbol_
   _ <- char '.'
   Abstraction symbol <$> parse_
 
-parseEvaluation_ :: Parsec String () Expression
+parseEvaluation_ :: Parser Expression
 parseEvaluation_ = do
   _ <- char '('
   e1 <- parse_
@@ -32,3 +32,5 @@ parseEvaluation_ = do
 
 instance Read Expression where
   readsPrec _ str = [(Lambda.Read.read str, "")]
+
+type Parser a = Parsec () String a
